75d5e5849e1883e68aabd70752940f6d2306f159
Hvford <34478667+Hvford@users.noreply.github.com>
2019-01-22 17:13:37 +0000

    Update Exiftool_phototimes.R

--- Exiftool_phototimes.R
+++ Exiftool_phototimes.R
@@ -5,16 +5,18 @@
 
 setwd("C:\\Exif")
 
-##reads metadata 
-data<-system2("exiftool", args="-common -DateTimeOriginal -FileType -GPS -csv -t -r C:\\Users\\Helen\\Documents\\PHD\\Data_Analysis\\PhotosforCOralNet\\HowBakPhotos\\Baker",stdout=TRUE)
+# Extract metadata 
+# Change file path to a folder containing only Jpegs, can have multiple folders within
+data<-system2("exiftool", args="-common -DateTimeOriginal -FileType -GPS -csv -t -r C:\\Users\\Helen\\Documents\\PHD\\Data_Analysis\\JARphotos",stdout=TRUE)
 
 #Check data
 str(data)
 head(data)
 
 #Save data
-write.csv(data,"C:\\Users\\Helen\\Documents\\PHD\\Data_Analysis\\Baker08\\Bakertimenew.csv")
+write.csv(data,"C:\\Users\\Helen\\Documents\\PHD\\Data_Analysis\\Jarvis 08\\Metadata\\Jarvis08_phototimes.csv")
 
 
 ##   Open dataframe in excel and edit
-##  Convert columns to text and clean up columns can save as a new data frame with just the transect photo name and date/time column
+##   Convert columns to text and clean up columns can save as a new data frame with just the transect photo name and date/time column
+##   In text to columns choose to separate by comma then delete irrelevant columns
